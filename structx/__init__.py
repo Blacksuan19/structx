3a6b5bb627f6af93cd64a0adf2f6334f071ed3d0
blacksuan19 <abubakaryagob@gmail.com>
2025-03-02 22:29:12 -0600

    Bump version 0.2.7 -> 0.2.8

--- structx/__init__.py
+++ structx/__init__.py
@@ -12,7 +12,7 @@ from structx.core.models import (
 )
 from structx.extraction.extractor import Extractor
 
-__version__ = "0.2.7"
+__version__ = "0.2.8"
 __all__ = [
     "Extractor",
     "ExtractionConfig",
